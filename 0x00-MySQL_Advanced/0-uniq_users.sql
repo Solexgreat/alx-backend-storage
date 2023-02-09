3271474c262350102580034a8c52d96ae3c97fe0
Solexg_great <solomonGodkid@gmail.com>
2023-02-09 22:34:22 +0000

    Task one and two

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- This sql script creates a new table called users
+
+CREATE TABLE IF NOT EXIST users (
+  id INT NOT NULL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  PRIMARY KEY (id)
+);
